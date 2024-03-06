97ae967add27efbc644219266d06b3a9c5866e5d
csUser <abbyhidalgo03@gmail.com>
2024-03-06 07:46:44 -0800

    fixed kill

--- work/Abigail/excercise_2.2.py
+++ work/Abigail/excercise_2.2.py
@@ -1,32 +1,20 @@
 import os
 import signal
 import time
+import sys
 
-def handle_input(pid):
+def handle_input():
     while True:
         try:
             val = input("Enter the process ID: ")
+            pid = os.getpid()
             if val == str(pid): # killing process
                 os.kill(pid, signal.SIGTERM)
-                print("Process with pid:", val, "killed successfully")
-                exit()
-            else:
-                print("Invalid value entered ")
+                print("Successfully killed process")
+                sys.exit(0)
         except EOFError:
             pass  # Ignore EOFError and keep waiting for input
 
-# Fork the process
-pid = os.fork()
 
-if pid < 0:
-    print("Error in fork")
-elif pid == 0: # Child process
-    parent_pid = os.getppid()
-    handle_input(parent_pid)
-else: # Parent process
-    print("Parent pid:", os.getppid())
-    child_pid = pid
-    print("Child pid:", child_pid)
-    time.sleep(2)  # Introduce a delay in parent process
-    handle_input(child_pid)
-
+time.sleep(1)  # Introduce a delay in parent process
+handle_input()
